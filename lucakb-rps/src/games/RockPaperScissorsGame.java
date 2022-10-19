7e56222fbdca5f052b2b7198027828334277008c
zgrkbr <ozgurkibar78@gmail.com>
2022-10-19 15:12:26 +0200

    revire version 0.1

--- lucakb-rps/src/games/RockPaperScissorsGame.java
+++ lucakb-rps/src/games/RockPaperScissorsGame.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class RockPaperScissorsGame {
 
+    //@TODO: If the fields are not declared as private they can be manipulated from outside
     Player player;
 
     Fist aIFist;
 
+
+    //@FIXME: declare the methods access modifier: public, private or protected
+    //An instance of the object cannot be created from outside its package if the constructor is not declared as public
     RockPaperScissorsGame(String playerName, int playerScore) {
         player = new Player(playerName, playerScore);
         aIFist = new Fist();
@@ -18,10 +22,14 @@ public class RockPaperScissorsGame {
         game.playGame();
     }
 
+    //@FIXME: declare the methods access modifier: public, private or protected
     void playGame() {
         System.out.println(player.getName());
 
         Scanner scanner = new Scanner(System.in);
+
+        //@FIXME: ctrl/cmd+alt+L
+        //@TODO: check IntelliJ's suggestion for the if condition and make adjustments
         for (int i = 0; i < 3; i++);
         int hand = aIFist.play();
         boolean playerChoice = true;
@@ -34,6 +42,8 @@ public class RockPaperScissorsGame {
             System.out.println("It's a draw");
 
         } else if (guess == 0 && hand == 2) {
+
+            //@TODO: check IntelliJ's suggestion for the if condition and make adjustments
             playerChoice = true;
             System.out.println("Rock beats Scissors, you win!");
 
